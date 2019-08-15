b7cd270da8884f08d33c8523a22ab4834a0e2a0f
ibyli <15186743693@163.com>
2019-08-15 21:27:30 +0800

    项目搭建

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <HomeNavbar />
+  </div>
+</template>
+
+<script>
+import HomeNavbar from './components/HomeNavbar.vue';
+
+export default {
+  components: {
+    HomeNavbar,
+  },
+};
+</script>
---
+++ src/views/Home/components/HomeNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <nav class="header__navbar">This is an about page</nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.header {
+  height: 55px;
+  background-color: #000;
+}
+</style>
